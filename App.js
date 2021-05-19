0cc5280bc47006d77eb46ea3a03b9ca777477882
Netanel Aknin <netanelos152@gmail.com>
2021-05-19 17:22:29 +0300

    first upload

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet, TextInput, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
+function Login(Email, Pass) {
+  alert("Succsessseddd aaaaaaa" + Email + Pass);
+}
+function LoginScreen() {
+  const [Email, onCangeEmail] = React.useState("Email");
+  const [Pass, onCangePass] = React.useState("Pass");
+  return (
+    
+      <SafeAreaView style={styles.container}>
+        <TextInput
+          style={styles.input}
+          onChangeText={onCangeEmail}
+          value={Email}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onCangePass}
+          value={Pass}
+        />
+        <Button
 
+          title="Press me"
+          onPress={() => Login(Email, Pass)}
+        />
+      </SafeAreaView>
+   
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 const styles = StyleSheet.create({
-  container: {
+    container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+    input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+  },
+
 });
+export default App;
