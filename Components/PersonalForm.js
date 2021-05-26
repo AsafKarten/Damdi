dc1321710bcc0ceaa80a3363ed23788e4a96c142
Netanel Aknin <netanelos152@gmail.com>
2021-05-26 15:11:08 +0300

    fix

--- Components/PersonalForm.js
+++ Components/PersonalForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-export default function PersonalForm() {
+export default function PersonalForm({navigation}) {
     return (
         <View>
             <Text>אנא מלא את הפרטים</Text>
