a2354d68409a99b25fc3d670c24b3ab50b2cfa50
Asaf Karten <70157456+AsafKarten@users.noreply.github.com>
2021-09-11 23:37:18 +0300

    imageeeee

--- client_screens/Welcome.js
+++ client_screens/Welcome.js
@@ -1,12 +1,13 @@
 
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { View, Text, Image } from 'react-native'
+import BG from '../assets/DAMDI_White_BG.jpg'
 export default function Welcome() {
 
     return (
         <View>
             <Text> ברוך הבא לדאמדי </Text>
+            <Image source={BG} style={{width:360, height: 150 , alignSelf:'center',resizeMode: 'stretch'}}></Image>
         </View>
     )
 }
