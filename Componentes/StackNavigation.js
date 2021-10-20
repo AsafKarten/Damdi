d97c48a2ad2241c086f4505c40dfc26e628e8bdb
Netanel Aknin <netanelos152@gmail.com>
2021-10-21 00:28:26 +0300

    split to 2 application

--- Componentes/StackNavigation.js
+++ Componentes/StackNavigation.js
@@ -4,29 +4,21 @@ import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Registration from '../client_screens/Registration'
-import Login from '../client_screens/Login.js'
-import PersonalFormA from '../client_screens/PersonalFormA';
-import PersonalFormB from '../client_screens/PersonalFormB';
-import PersonalFormC from '../client_screens/PersonalFormC';
-import Welcome from '../client_screens/Welcome.js';
-import Home from '../client_screens/Home.js';
-import Stations from '../client_screens/Stations.js';
-import Profile from '../client_screens/Profile.js';
-import Appointments from '../client_screens/Appointments.js';
-import Friends from '../client_screens/Friends.js';
-import BloodInfo from '../client_screens/BloodInfo.js';
-import ScheduleAppointment from '../client_screens/ScheduleAppointment.js';
-import MedicalForm from '../client_screens/MedicalForm.js';
-import ValidationForm from '../client_screens/ValidationForm.js';
-import DonatorsLogin from '../donator_screens/DonatorsLogin.js';
-import DHome from '../donator_screens/DHome.js';
-import UnitOne from '../donator_screens/UnitOne.js';
-import UnitTwo from '../donator_screens/UnitTwo.js';
-import UnitThree from '../donator_screens/UnitThree.js';
-import DonorInfo from '../donator_screens/DonorInfo.js';
-import PersonalInfo from '../donator_screens/PersonalInfo.js';
-import MedicalInfo from '../donator_screens/MedicalInfo.js';
+import Registration from '../ClientScreens/Registration'
+import Login from '../ClientScreens/Login.js'
+import PersonalFormA from '../ClientScreens/PersonalFormA';
+import PersonalFormB from '../ClientScreens/PersonalFormB';
+import PersonalFormC from '../ClientScreens/PersonalFormC';
+import Welcome from '../ClientScreens/Welcome.js';
+import Home from '../ClientScreens/Home.js';
+import Stations from '../ClientScreens/Stations.js';
+import Profile from '../ClientScreens/Profile.js';
+import Appointments from '../ClientScreens/Appointments.js';
+import Friends from '../ClientScreens/Friends.js';
+import BloodInfo from '../ClientScreens/BloodInfo.js';
+import ScheduleAppointment from '../ClientScreens/ScheduleAppointment.js';
+import MedicalForm from '../ClientScreens/MedicalForm.js';
+import ValidationForm from '../ClientScreens/ValidationForm.js';
 
 const Stack = createStackNavigator();
 
@@ -205,102 +197,6 @@ export default function StackNavigation() {
         <Stack.Screen name="MedicalForm" component={MedicalForm}
           options={{
             title: 'טופס רפואי',
-            headerStyle: {
-              backgroundColor: '#7d91b0',
-            },
-            headerTintColor: '#fff'
-            // headerRight: () => <Feather onPress={() => backPage('BloodInfo')} name="arrow-right-circle" size={32} color="grey" style={styles.arrow} />,
-            // headerLeft: () => {
-            //   return null;
-            // },
-          }} />
-        <Stack.Screen name="DonatorsLogin" component={DonatorsLogin}
-          options={{
-            title: 'התחברות מתרים',
-            headerStyle: {
-              backgroundColor: '#7d91b0',
-            },
-            headerTintColor: '#fff'
-            // headerRight: () => <Feather onPress={() => backPage('BloodInfo')} name="arrow-right-circle" size={32} color="grey" style={styles.arrow} />,
-            // headerLeft: () => {
-            //   return null;
-            // },
-          }} />
-        <Stack.Screen name="DHome" component={DHome}
-          options={{
-            title: 'מסך בית מתרים',
-            headerStyle: {
-              backgroundColor: '#7d91b0',
-            },
-            headerTintColor: '#fff'
-            // headerRight: () => <Feather onPress={() => backPage('BloodInfo')} name="arrow-right-circle" size={32} color="grey" style={styles.arrow} />,
-            // headerLeft: () => {
-            //   return null;
-            // },
-          }} />
-        <Stack.Screen name="UnitOne" component={UnitOne}
-          options={{
-            title: 'עמדה 1 - קבלת תורמים',
-            headerStyle: {
-              backgroundColor: '#7d91b0',
-            },
-            headerTintColor: '#fff'
-            // headerRight: () => <Feather onPress={() => backPage('BloodInfo')} name="arrow-right-circle" size={32} color="grey" style={styles.arrow} />,
-            // headerLeft: () => {
-            //   return null;
-            // },
-          }} />
-        <Stack.Screen name="UnitTwo" component={UnitTwo}
-          options={{
-            title: 'עמדה 2 - בדיקה רפואית',
-            headerStyle: {
-              backgroundColor: '#7d91b0',
-            },
-            headerTintColor: '#fff'
-            // headerRight: () => <Feather onPress={() => backPage('BloodInfo')} name="arrow-right-circle" size={32} color="grey" style={styles.arrow} />,
-            // headerLeft: () => {
-            //   return null;
-            // },
-          }} />
-        <Stack.Screen name="UnitThree" component={UnitThree}
-          options={{
-            title: 'עמדה 3 - לקיחת תרומות',
-            headerStyle: {
-              backgroundColor: '#7d91b0',
-            },
-            headerTintColor: '#fff'
-            // headerRight: () => <Feather onPress={() => backPage('BloodInfo')} name="arrow-right-circle" size={32} color="grey" style={styles.arrow} />,
-            // headerLeft: () => {
-            //   return null;
-            // },
-          }} />
-        <Stack.Screen name="DonorInfo" component={DonorInfo}
-          options={{
-            title: 'פרטים אישיים תורם',
-            headerStyle: {
-              backgroundColor: '#7d91b0',
-            },
-            headerTintColor: '#fff'
-            // headerRight: () => <Feather onPress={() => backPage('BloodInfo')} name="arrow-right-circle" size={32} color="grey" style={styles.arrow} />,
-            // headerLeft: () => {
-            //   return null;
-            // },
-          }} />
-        <Stack.Screen name="MedicalInfo" component={MedicalInfo}
-          options={{
-            title: 'פרטים רפואים תורם',
-            headerStyle: {
-              backgroundColor: '#7d91b0',
-            },
-            headerTintColor: '#fff'
-            // headerRight: () => <Feather onPress={() => backPage('BloodInfo')} name="arrow-right-circle" size={32} color="grey" style={styles.arrow} />,
-            // headerLeft: () => {
-            //   return null;
-            // },
-          }} />
-        <Stack.Screen name="PersonalInfo" component={PersonalInfo}
-          options={{
-            title: 'פרטים אישיים תורם',
             headerStyle: {
               backgroundColor: '#7d91b0',
             },
