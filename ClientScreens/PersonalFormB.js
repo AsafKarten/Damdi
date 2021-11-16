22a257c2ec6600c737f277eb53cb3488d02fb650
Netanel Aknin <netanelos152@gmail.com>
2021-11-16 22:56:28 +0200

    fix autocompleate

--- ClientScreens/PersonalFormB.js
+++ ClientScreens/PersonalFormB.js
@@ -37,14 +37,16 @@ export default function PersonalFormB({ navigation, route }) {
     User.Mail_box = mailBox
     User.Telephone = telephone
     User.Work_telephone = workTelephone
-    var UserB = {Personal_id : route.params.route.Personal_id, First_name : route.params.route.First_name, Last_name: route.params.route.Last_name,Phone:route.params.route.Phone, Gender : route.params.route.Gender, Birthdate: route.params.route.Birthdate, Prev_first_name: route.params.route.Prev_first_name, Prev_last_name: route.params.route.Prev_last_name,
-      City: city, Address:address, Postal_code: postalCode, Mail_box:mailBox, Telephone:telephone, Work_telephone:workTelephone}
-    console.log("PersonalFormB",UserB);
+    var UserB = {
+      Personal_id: route.params.route.Personal_id, First_name: route.params.route.First_name, Last_name: route.params.route.Last_name, Phone: route.params.route.Phone, Gender: route.params.route.Gender, Birthdate: route.params.route.Birthdate, Prev_first_name: route.params.route.Prev_first_name, Prev_last_name: route.params.route.Prev_last_name,
+      City: city, Address: address, Postal_code: postalCode, Mail_box: mailBox, Telephone: telephone, Work_telephone: workTelephone
+    }
+    console.log("PersonalFormB", UserB);
     setLoading(false);
     navigation.navigate('PersonalFormC', { route: UserB })
   }
 
-    const serachCity = async (q) => {
+  const serachCity = async (q) => {
     let url = `https://data.gov.il/api/3/action/datastore_search?resource_id=351d4347-8ee0-4906-8e5b-9533aef13595&q=${q}`
     let res = await fetch(url);
     let data = await res.json();
@@ -52,9 +54,10 @@ export default function PersonalFormB({ navigation, route }) {
     setCities(data.result.records)
   }
 
-    useEffect(()=>{
-        serachCity(city)
-    },[city])
+  useEffect(() => {
+    serachCity(city)
+  }, [city])
+  
   return (
     <SafeAreaView style={styles.container}>
       <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
@@ -69,88 +72,91 @@ export default function PersonalFormB({ navigation, route }) {
                 placeholder="עיר"
                 maxLength={20}
               />
-            <View>
-                {cities.length >0 ? cities.map(item=>
-                <TouchableOpacity onPress={()=> setCity(item["שם יישוב"]) } >
-                <Text>{item["שם יישוב"]}</Text>
-                </TouchableOpacity> )
-                :null}
+              <View>
+                {cities.length > 0 ? cities.map(item =>
+                  <TouchableOpacity onPress={() => setCity(item["שם יישוב"])} >
+                    <View style={styles.button_normal}>
+                      <Text style={styles.button_text}>{item["שם יישוב"]}</Text>
+                    </View>
+                  </TouchableOpacity>)
+                  : null}
+              </View>
             </View>
-            </View>
-            <View style={styles.HorizontalBox}>
-              <Text style={styles.lableText}>רחוב</Text>
-              <TextInput
-                style={styles.input}
-                onChangeText={setAddress}
-                value={address}
-                placeholder="רחוב"
-                maxLength={30}
-              />
-            </View>
-            <View style={styles.HorizontalBox}>
-              <Text style={styles.lableText}>מיקוד</Text>
-              <TextInput
-                style={styles.input}
-                onChangeText={setPostalCode}
-                value={postalCode}
-                placeholder="מיקוד"
-                keyboardType='numeric'
-                maxLength={7}
-              />
-            </View>
-            <View style={styles.HorizontalBox}>
-              <Text style={styles.lableText}>תיבת דואר</Text>
-              <TextInput
-                keyboardType='numeric'
-                style={styles.input}
-                onChangeText={setMailBox}
-                value={mailBox}
-                placeholder="תיבת דואר"
-                maxLength={6}
-              />
-            </View>
-            <View style={styles.HorizontalBox}>
-              <Text style={styles.lableText}>מס טלפון</Text>
-              <TextInput
-                keyboardType='numeric'
-                style={styles.input}
-                onChangeText={setTelephone}
-                value={telephone}
-                placeholder="מס טלפון"
-                maxLength={9}
-              />
-            </View>
-            <View style={styles.HorizontalBox}>
-              <Text style={styles.lableText}>מס טלפון בעבודה</Text>
-              <TextInput
-                style={styles.input}
-                onChangeText={setWorkTelephone}
-                value={workTelephone}
-                placeholder="מס טלפון בעבודה"
-                keyboardType='numeric'
-                maxLength={9}
-              />
-            </View>
-            <View style={styles.HorizontalBoxButtons}>
-              <TouchableOpacity onPress={() => {
-                setLoading(false)
-                PostPersonalFormB()
-              }}>
-                <View style={styles.button_normal}>
-                  <Text style={styles.button_text} >הבא</Text>
-                </View>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => navigation.navigate('PersonalFormA')} >
-                <View style={styles.button_normal}>
-                  <Text style={styles.button_text} >חזרה</Text>
-                </View>
-              </TouchableOpacity>
-            </View>
-            {loading && <Spiner loading={loading} />}
           </View>
-        </TouchableWithoutFeedback>
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+          <View style={styles.HorizontalBox}>
+            <Text style={styles.lableText}>רחוב</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={setAddress}
+              value={address}
+              placeholder="רחוב"
+              maxLength={30}
+            />
+          </View>
+          <View style={styles.HorizontalBox}>
+            <Text style={styles.lableText}>מיקוד</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={setPostalCode}
+              value={postalCode}
+              placeholder="מיקוד"
+              keyboardType='numeric'
+              maxLength={7}
+            />
+          </View>
+          <View style={styles.HorizontalBox}>
+            <Text style={styles.lableText}>תיבת דואר</Text>
+            <TextInput
+              keyboardType='numeric'
+              style={styles.input}
+              onChangeText={setMailBox}
+              value={mailBox}
+              placeholder="תיבת דואר"
+              maxLength={6}
+            />
+          </View>
+          <View style={styles.HorizontalBox}>
+            <Text style={styles.lableText}>מס טלפון</Text>
+            <TextInput
+              keyboardType='numeric'
+              style={styles.input}
+              onChangeText={setTelephone}
+              value={telephone}
+              placeholder="מס טלפון"
+              maxLength={9}
+            />
+          </View>
+          <View style={styles.HorizontalBox}>
+            <Text style={styles.lableText}>מס טלפון בעבודה</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={setWorkTelephone}
+              value={workTelephone}
+              placeholder="מס טלפון בעבודה"
+              keyboardType='numeric'
+              maxLength={9}
+            />
+          </View>
+          <View style={styles.HorizontalBoxButtons}>
+            <TouchableOpacity onPress={() => {
+              setLoading(false)
+              PostPersonalFormB()
+            }}>
+              <View style={styles.button_normal}>
+                <Text style={styles.button_text} >הבא</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate('PersonalFormA')} >
+              <View style={styles.button_normal}>
+                <Text style={styles.button_text} >חזרה</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          {loading && <Spiner loading={loading} />}
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+    </SafeAreaView >
   );
 
 }
